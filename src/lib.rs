18df7f452968f517b4c7a87d0c27cc5b7ee20bd5
Scott McLeod <scotmcle@cisco.com>
2019-09-06 16:38:07 -0400

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,33 @@
 
 /// Function inputs will be column types in database ordinal order -> iterator that will return bytes for vnf header
 
-#[allow(dead_code)]
-static SIGNATURE: [u8; 11] = [0x4E, 0x41, 0x54, 0x49, 0x56, 0x45, 0x0A, 0xFF, 0x0D, 0x0A, 0x00];
+const SIGNATURE: [u8; 11] = [0x4E, 0x41, 0x54, 0x49, 0x56, 0x45, 0x0A, 0xFF, 0x0D, 0x0A, 0x00];
+const HEADER_AREA_LENGTH: [u8; 4] = [0x3D, 0x0, 0x0, 0x0];
+const VERSION: [u8; 2] = [0x01, 0x00];
+const FILLER: u8 = 0x00;
+
+#[derive(Debug)]
+struct Header {
+    signature: [u8; 11],
+    header_area_length: [u8; 4],
+    version: [u8; 2],
+    filler: u8,
+    columns: [u8; 2],
+
+}
 
 #[allow(dead_code)]
-fn header() -> [u8; 11] {
-    SIGNATURE
+fn header() -> Header {
+    Header { signature: SIGNATURE,
+             header_area_length: HEADER_AREA_LENGTH,
+             version: VERSION,
+             filler: FILLER,
+             columns: [0x0E, 0x00],
+
+    }
 }
+
+#[allow(dead_code)]
 
 #[cfg(test)]
 mod tests {
@@ -16,6 +36,7 @@ mod tests {
 
     #[test]
     fn another_test() {
-        assert_eq!(SIGNATURE, header());
+        println!("{:?}", header());
+        // assert_eq!(SIGNATURE, header());
     }
 }
