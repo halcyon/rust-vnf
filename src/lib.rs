1a7fb04e6fe5755a5b4970dec1d94cd2676e4026
Scott McLeod <scotmcle@cisco.com>
2019-09-06 15:48:00 -0400

    Allow dead code

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 /// Function inputs will be column types in database ordinal order -> iterator that will return bytes for vnf header
 
 #[allow(dead_code)]
-
 static SIGNATURE: [u8; 11] = [0x4E, 0x41, 0x54, 0x49, 0x56, 0x45, 0x0A, 0xFF, 0x0D, 0x0A, 0x00];
 
+#[allow(dead_code)]
 fn header() -> [u8; 11] {
     SIGNATURE
 }
