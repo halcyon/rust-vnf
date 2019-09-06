b7f0a4aa9785daed3c2125aabdebfd55c5e4b3b3
Scott McLeod <scotmcle@cisco.com>
2019-09-06 17:17:53 -0400

    Add header struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+
+
 /// Function inputs will be column types in database ordinal order -> vnf header in byte array format.
 
 /// Function inputs will be column types in database ordinal order -> iterator that will return bytes for vnf header
@@ -7,14 +10,24 @@ const HEADER_AREA_LENGTH: [u8; 4] = [0x3D, 0x0, 0x0, 0x0];
 const VERSION: [u8; 2] = [0x01, 0x00];
 const FILLER: u8 = 0x00;
 
-#[derive(Debug)]
+
+// #[derive(Debug)]
 struct Header {
     signature: [u8; 11],
     header_area_length: [u8; 4],
     version: [u8; 2],
     filler: u8,
     columns: [u8; 2],
+}
 
+impl fmt::Display for Header {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "signature: {:X?}", self.signature)?;
+        writeln!(f, "header_area_length: {:X?}", self.header_area_length)?;
+        writeln!(f, "version: {:X?}", self.version)?;
+        writeln!(f, "filler: {:X?}", self.filler)?;
+        writeln!(f, "columns: {:X?}", self.columns)
+    }
 }
 
 #[allow(dead_code)]
@@ -24,7 +37,6 @@ fn header() -> Header {
              version: VERSION,
              filler: FILLER,
              columns: [0x0E, 0x00],
-
     }
 }
 
@@ -36,7 +48,7 @@ mod tests {
 
     #[test]
     fn another_test() {
-        println!("{:?}", header());
+        println!("{}", header());
         // assert_eq!(SIGNATURE, header());
     }
 }
